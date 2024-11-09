e7418441543e1cd9924dde501b6235dfa1842be1
M0hamady <119875626+M0hamady@users.noreply.github.com>
2024-11-09 17:54:03 +0200

    update in loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingComponent: React.FC = () => {
   return (
-    <div className='flex justify-center items-center w-full h-[100vh] max-w-[90vw] '>
+    <div className='flex justify-center items-center w-full h-[100vh] max-w-[99vw] '>
       <div className="spinner-container">
         <div className="spinner"></div>
         <div className="text-container text-primary mt-2">
